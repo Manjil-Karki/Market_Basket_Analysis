bbb238ca2f7dbb04fee528e4e8129f0589ea91f0
Manjil Karki <kmanzil10@gmail.com>
2022-07-29 22:18:32 +0545

    Added Frequent Pattern growth algo

--- Freq_pattern_growth.py
+++ Freq_pattern_growth.py
@@ -24,9 +24,20 @@ def calc_order(unordered, priority):
 
 
 def order_items(df, priority):
-    df["Oreder_Items"] = df["Items"].apply(calc_order, args = [priority])
+    df["Order_Items"] = df["Items"].apply(calc_order, args = [priority])
     return df
 
+
+def create_graph_structure(df, order):
+    ordered_df = pd.DataFrame()
+    max_width = df["Order_Items"].apply(lambda x : len(x)).max()
+    cols = [f"order_{i}" for i in range(0, max_width)]
+    ordered_df[cols] = pd.DataFrame(df.Order_Items.to_list(), index = df.index)
+    ordered_df.replace(order, range(0, len(order)), inplace = True)
+    ordered_df.sort_values(cols, inplace = True)
+    ordered_df.replace(range(0, len(order)), order, inplace = True)
+    ordered_df.reset_index(inplace = True)
+    return ordered_df
 
 
 if __name__ == "__main__":
@@ -45,4 +56,9 @@ if __name__ == "__main__":
     min_support = min_support / 100
 
     freq_df = calculate_priority(df, min_support)
+    # print(freq_df)
     df = order_items(df, freq_df["Items"].to_list())
+
+    # print(df)
+    final_structure_df = create_graph_structure(df, freq_df["Items"].to_list())
+    print(final_structure_df)
